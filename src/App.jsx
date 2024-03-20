6fcd4d8a0806a28fb73898e5a6bf0536c569ab0b
quocbaole255 <lenguyenquocbaotqt2016@gmail.com>
2024-03-20 14:18:02 +0700

    code layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,4 @@
-import { AccessAlarm, Home, ThreeDRotation } from "@mui/icons-material";
-import { Button } from "@mui/material";
-import Typography from "@mui/material/Typography";
 import { useColorScheme } from "@mui/material/styles";
-import useMediaQuery from "@mui/material/useMediaQuery";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
@@ -11,6 +7,7 @@ import Box from "@mui/material/Box";
 import LightModeIcon from "@mui/icons-material/LightMode";
 import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
 import SettingsBrightnessIcon from "@mui/icons-material/SettingsBrightness";
+import Container from "@mui/material/Container";
 
 function ModeSelect() {
   const { mode, setMode } = useColorScheme();
@@ -43,7 +40,7 @@ function ModeSelect() {
           </Box>
         </MenuItem>
         <MenuItem value="system">
-          <Box sx={{ display: "flex", alightItem: "center", gap: 2 }}>
+          <Box sx={{ display: "flex", alightItem: "center", gap: 1 }}>
             <SettingsBrightnessIcon fontSize="small" />
             System
           </Box>
@@ -53,44 +50,44 @@ function ModeSelect() {
   );
 }
 
-function ModeToggle() {
-  const { mode, setMode } = useColorScheme();
-  // const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
-  return (
-    <Button
-      onClick={() => {
-        setMode(mode === "light" ? "dark" : "light");
-      }}
-    >
-      {mode === "light" ? "Turn dark" : "Turn light"}
-    </Button>
-  );
-}
-
 function App() {
   return (
-    <>
-      <ModeSelect />
-      <hr />
-      <ModeToggle />
-      <hr />
-      <div>Trung quan dev</div>
-
-      <Typography variant="body2" color="text.secondary">
-        Test Typography
-      </Typography>
-
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Hello World</Button>
-      <Button variant="outlined">Outlined</Button>
-
-      <br />
-      <AccessAlarm />
-      <ThreeDRotation />
-      <Home color="primary" />
-      <Home color="secondary" />
-      <Home color="success" />
-    </>
+    <Container disableGutters maxWidth={false} sx={{ height: "100vh" }}>
+      <Box
+        sx={{
+          backgroundColor: "primary.light",
+          width: "100%",
+          height: (theme) => theme.trello.appBarHeight,
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <ModeSelect />
+      </Box>
+      <Box
+        sx={{
+          backgroundColor: "primary.dark",
+          width: "100%",
+          height: (theme) => theme.trello.boardBarHeight,
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        Board Bar
+      </Box>
+      <Box
+        sx={{
+          backgroundColor: "primary.main",
+          width: "100%",
+          height: (theme) =>
+            `calc(100vh - ${theme.trello.appBarHeight} - ${theme.trello.boardBarHeight})`,
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        Board Content
+      </Box>
+    </Container>
   );
 }
 
